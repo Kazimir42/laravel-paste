ad0eb21e7aaeeb27248ab624e71c00fab0b6d939
Remi <champlonremii@gmail.com>
2021-08-19 09:48:28 +0200

    adding button creation and prepare for public paste featurs

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,10 +13,13 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('pastes.index')" :active="request()->routeIs('pastes.index')">
-                        {{ __('Pastes') }}
+                        {{ __('My Pastes') }}
                     </x-nav-link>
                     <x-nav-link :href="route('pastes.create')" :active="request()->routeIs('pastes.create')">
-                        {{ __('Creation') }}
+                        {{ __('New Paste') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('pastes.create')" :active="request()->routeIs('pastes.create')">
+                        {{ __('Public Pastes') }}
                     </x-nav-link>
                 </div>
             </div>
--- resources/views/pastes/index.blade.php
+++ resources/views/pastes/index.blade.php
@@ -7,8 +7,16 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-second overflow-hidden shadow-sm sm:rounded-lg mb-3">
+                <div class="">
+                    <a href="{{ route('pastes.create') }}">
+                        <button class="mx-6 my-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded float-right">
+                            + New Paste
+                        </button>
+                    </a>
+                </div>
+            </div>
             @foreach($pastes ?? '' as $paste)
-
                 <div class="bg-white dark:bg-second overflow-hidden shadow-sm sm:rounded-lg mb-3">
                     <div class="p-6">
                         <h3 class="font-semibold text-xl text-gray-300 leading-tight ">{{ $paste->title }}</h3>
