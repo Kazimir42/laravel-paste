8dc700d4c68a70f3e8895284a876034629c2375a
Remi <champlonremii@gmail.com>
2021-08-20 14:44:46 +0200

    response menu update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -133,8 +133,8 @@
         <!-- Responsive Settings Options -->
             <div class="pt-4 pb-1 border-t border-gray-200">
                 <div class="px-4">
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    <div class="font-medium text-base dark:text-gray-300 text-gray-800">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm dark:text-gray-300 text-gray-500">{{ Auth::user()->email }}</div>
                 </div>
 
                 <div class="mt-3 space-y-1">
