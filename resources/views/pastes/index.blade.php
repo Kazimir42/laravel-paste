0c79a58778b6ac2e2a59db4088e939036abf8b2d
kazi <champlonremii@gmail.com>
2021-11-20 20:49:38 +0100

    design update

--- resources/views/pastes/index.blade.php
+++ resources/views/pastes/index.blade.php
@@ -54,7 +54,7 @@
                     </div>
                 </div>
             @endforeach
-            
+
             <div class="mx-auto">
                 {!! $pastes->links() !!}
             </div>
